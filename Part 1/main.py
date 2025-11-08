f7934bcac2a941cf57591c999ac737608ded8165
aina <ainavarro1254@gmail.com>
2025-11-08 15:44:54 +0100

    simple preprocessing added + simple DQN also constructed

--- Part 1/main.py
+++ Part 1/main.py
@@ -1,11 +1,13 @@
 import gymnasium as gym
-
-# from preprocessing import *
+from preprocessing import *
 
 
 NAME_ENV = "PongNoFrameskip-v4"
 
 if __name__ == "__main__":
     # env = make_env(NAME_ENV)
-    env = gym.make(NAME_ENV)
-    print(env.action_space)
+    # env = gym.make(NAME_ENV)
+    # print(env.action_space)
+
+    env = make_env(NAME_ENV)
+    print_env_info("Wrapped", env)
--- Part 1/preprocessing.py
+++ Part 1/preprocessing.py
@@ -1,51 +1,55 @@
-import typing as tt
-import gymnasium as gym
-from gymnasium import spaces
-import collections
 import numpy as np
-# from stable_baselines3.common import atari_wrappers
+import gymnasium as gym
+from gymnasium.wrappers import MaxAndSkipObservation, ResizeObservation, GrayscaleObservation, FrameStackObservation, ReshapeObservation
+import ale_py
+
+class ImageToPyTorch(gym.ObservationWrapper):
+    def __init__(self, env):
+        super().__init__(env)
+        old_shape = self.observation_space.shape
+        self.observation_space = gym.spaces.Box(low=0.0, high=1.0, shape=(old_shape[-1], old_shape[0], old_shape[1]), dtype=np.float32) 
+        # change the order of the matrix (frame). For using torch we need to swap the dimensions
+
+    def observation(self, observation):
+        return np.moveaxis(observation, 2, 0)
 
 
-# class ImageToPyTorch(gym.ObservationWrapper):
-#     def __init__(self, env):
-#         super(ImageToPyTorch, self).__init__(env)
-#         obs = self.observation_space
-#         assert isinstance(obs, gym.spaces.Box)
-#         assert len(obs.shape) == 3
-#         new_shape = (obs.shape[-1], obs.shape[0], obs.shape[1])
-#         self.observation_space = gym.spaces.Box(
-#             low=obs.low.min(), high=obs.high.max(),
-#             shape=new_shape, dtype=obs.dtype)
-
-#     def observation(self, observation):
-#         return np.moveaxis(observation, 2, 0)
+class ScaledFloatFrame(gym.ObservationWrapper):
+    def observation(self, obs):
+        return np.array(obs).astype(np.float32) / 255.0
+    # change the range of values to [0,1]
 
 
-# class BufferWrapper(gym.ObservationWrapper):
-#     def __init__(self, env, n_steps):
-#         super(BufferWrapper, self).__init__(env)
-#         obs = env.observation_space
-#         assert isinstance(obs, spaces.Box)
-#         new_obs = gym.spaces.Box(
-#             obs.low.repeat(n_steps, axis=0), obs.high.repeat(n_steps, axis=0),
-#             dtype=obs.dtype)
-#         self.observation_space = new_obs
-#         self.buffer = collections.deque(maxlen=n_steps)
-
-#     def reset(self, *, seed: tt.Optional[int] = None, options: tt.Optional[dict[str, tt.Any]] = None):
-#         for _ in range(self.buffer.maxlen-1):
-#             self.buffer.append(self.env.observation_space.low)
-#         obs, extra = self.env.reset()
-#         return self.observation(obs), extra
-
-#     def observation(self, observation: np.ndarray) -> np.ndarray:
-#         self.buffer.append(observation)
-#         return np.concatenate(self.buffer)
+def print_env_info(name, env):
+    obs, _ = env.reset()
+    print("*** {} Environment ***".format(name))
+    print("Environment obs. : {}".format(env.observation_space.shape))
+    print("Observation shape: {}, type: {} and range [{},{}]".format(obs.shape, obs.dtype, np.min(obs), np.max(obs)))
+    print("Observation sample:\n{}".format(obs))
 
 
-def make_env(env_name: str, **kwargs):
-    env = gym.make(env_name, **kwargs)
-    # env = atari_wrappers.AtariWrapper(env, clip_reward=False, noop_max=0)
-    # env = ImageToPyTorch(env)
-    # env = BufferWrapper(env, n_steps=4)
+
+def make_env(env_name):
+    env = gym.make(env_name) # get the environment
+    print("Standard Env.        : {}".format(env.observation_space.shape)) 
+    env = MaxAndSkipObservation(env, skip=4) # all frames too similar, then take one framework every 4
+    print("MaxAndSkipObservation: {}".format(env.observation_space.shape))
+    #env = FireResetEnv(env) # starting some of the atari games
+    env = ResizeObservation(env, (84, 84)) # define the 84x84 frames for the observations
+    print("ResizeObservation    : {}".format(env.observation_space.shape))
+    env = GrayscaleObservation(env, keep_dim=True) # convert observation to gray scale
+    print("GrayscaleObservation : {}".format(env.observation_space.shape))
+    env = ImageToPyTorch(env) # image to pytorch
+    print("ImageToPyTorch       : {}".format(env.observation_space.shape))
+    env = ReshapeObservation(env, (84, 84)) # remove the first dimensions
+    print("ReshapeObservation   : {}".format(env.observation_space.shape))
+    env = FrameStackObservation(env, stack_size=4) # stack the last four frames for keeping the dynamics
+    print("FrameStackObservation: {}".format(env.observation_space.shape))
+    env = ScaledFloatFrame(env) # scale the frames of the environment
+    print("ScaledFloatFrame     : {}".format(env.observation_space.shape))
+    
     return env
+
+
+
+
